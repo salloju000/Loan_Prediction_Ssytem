c87475a9330d609a332fe99fd4882d076b860e43
salloju.pruthvi raj <salloju.sidharth30@gmail.com>
2026-02-22 14:54:00 +0530

    vercel analytics

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -13,6 +13,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'   // global styles, Tailwind base layers, CSS variables
 import App from './App' // root application component
 
@@ -22,6 +23,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 )
